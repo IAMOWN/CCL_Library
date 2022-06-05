70a0199cbf3a5fd0d93f96c5711d72da3d818ec3
Carewen <carewen@protonmail.com>
2022-06-05 17:29:37 -0600

    Update settings.py

--- CCL_Library/settings.py
+++ CCL_Library/settings.py
@@ -110,11 +110,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # STATIC_URL = 'static/'
-STATIC_URL = 'https://ega.s3.us-east-2.amazonaws.com/ccl-library-static/'
+# STATIC_URL = 'https://ega.s3.us-east-2.amazonaws.com/ccl-library-static/'
+STATIC_URL = 'https://django-whurthy.s3.us-west-1.amazonaws.com/whurthy-static/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
