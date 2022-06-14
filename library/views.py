2357b8d9da7c94e85d3db183f78d10df97458d7f
Carewen <carewen@protonmail.com>
2022-06-14 17:00:03 -0600

    Update views.py

--- library/views.py
+++ library/views.py
@@ -628,8 +628,8 @@ class LibraryRecordDetail(DetailView):
                 part_numbers.append(record.part_number)
             context['first_part_number'] = min(part_numbers)
             context['last_part_number'] = max(part_numbers)
-            context['previous'] = int(libary_record.part_number) - 1
-            context['next'] = int(libary_record.part_number) + 1
+            context['previous'] = LibraryRecord.objects.get(pk=int(libary_record.part_number) - 1)
+            context['next'] = LibraryRecord.objects.get(pk=int(libary_record.part_number) + 1)
             context['series'] = True
         else:
             context['series'] = False
