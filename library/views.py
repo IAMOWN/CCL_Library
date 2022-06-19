2622186f7ff598f75cb18093aefb26eb4e7ca4ef
Carewen <carewen@protonmail.com>
2022-06-19 11:22:01 -0600

    Update views.py

--- library/views.py
+++ library/views.py
@@ -1278,7 +1278,9 @@ class ReadingList(LoginRequiredMixin, ListView):
 
         record_types = []
         reading_progress_obj = ReadingProgress.objects.filter(dear_soul__username=self.request.user)
+        print(f'reading_progress_obj: {reading_progress_obj}')
         for record in reading_progress_obj:
+            print(f'record: {record}')
             current_record_type = LibraryRecord.objects.get(title=record.record).library_record_type
             record_types.append(current_record_type)
         print(f'record_types: {record_types}')
