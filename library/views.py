8379916d300ed3157f11ae4560c542fecf47fc5b
Carewen <carewen@protonmail.com>
2022-06-18 19:43:45 -0600

    Update views.py

--- library/views.py
+++ library/views.py
@@ -669,6 +669,7 @@ class LibraryRecordDetail(DetailView):
         selected_reading_progress = self.request.GET.get('reading-progress') or ''
 
         if current_reading_progress == '---------' and selected_reading_progress == '1) On Reading List':
+            reading_progress_obj = ReadingProgress.objects.get(dear_soul__username=self.request.user, record_id=self.kwargs['pk'])
             reading_progress_obj.date_added = get_current_date()
             reading_progress_obj.reading_progress = '1) On Reading List'
             reading_progress_obj.save(update_fields=['date_added', 'reading_progress'])
