e807fffdadb9bb1cb88dcf20dc3c88593073deaa
Carewen <carewen@protonmail.com>
2022-06-27 00:55:33 -0600

    Refine Record Observation

--- library/views.py
+++ library/views.py
@@ -1702,7 +1702,11 @@ def record_observation(request, pk):
             'name': observer,
             'valid': True,
             'confirm_message_1': "Beloved ",
-            'confirm_message_2': '''We thank you for taking the time to share this Observation with us.<br>The Digital Librarian Circle of Light have been assigned a task and will be looking into this.<p>Love and Blessings,<br>GRACE (Guiding Responsive Assistant for Curriculum and Education)''',
+            'confirm_message_2': '''We thank you for taking the time to share this Observation with us.''',
+            'confirm_message_3': '''The Digital Librarian Circle of Light have been assigned a task and will be looking into this.''',
+            'confirm_message_4': '''Love and Blessings,''',
+            'confirm_message_5': '''GRACE (Guiding Responsive Assistant for Curriculum and Education)''',
+
             'pk': pk,
         }
 
