3d6755dc0a21eee9c702f3e2a94397008cb38a4f
Carewen <carewen@protonmail.com>
2022-07-06 16:58:48 -0600

    Update admin.py

--- iamown/admin.py
+++ iamown/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import (
     Task,
     ServiceGroup,
+    LEE,
 )
 
 admin.site.register(Task)
 admin.site.register(ServiceGroup)
+admin.site.register(LEE)
