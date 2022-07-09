3c3c91b5398582e673fb574307afa2dc66d61077
Carewen <carewen@protonmail.com>
2022-07-09 13:50:28 -0600

    Update views.py

--- iamown/views.py
+++ iamown/views.py
@@ -1282,9 +1282,7 @@ class EmailCampaignCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateVie
     def get_context_data(self, *args, **kwargs):
         context = super(EmailCampaignCreateView, self).get_context_data(**kwargs)
         context['page_type'] = 'Create'
-        form_instructions = LEE.objects.get(task_name='Email Campaign Start (1)')
-        print(f'{form_instructions}')
-        context['form_instructions'] = form_instructions
+        context['form_instructions'] = LEE.objects.get(task_name='Email Campaign Start (1)').process_description
         return context
 
 
