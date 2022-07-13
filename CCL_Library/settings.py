75a4e27691a07e94117480508a7fa017fcf12348
Carewen <carewen@protonmail.com>
2022-07-13 10:22:52 -0600

    Update settings.py

--- CCL_Library/settings.py
+++ CCL_Library/settings.py
@@ -5,7 +5,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 CSRF_COOKIE_SECURE = True
