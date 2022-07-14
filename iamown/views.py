3d70652222da5bcaa44044a2b57fb6115ee8eb96
Carewen <carewen@protonmail.com>
2022-07-14 13:32:38 -0600

    Fix entry.mailing_list_log update

--- iamown/views.py
+++ iamown/views.py
@@ -1706,7 +1706,7 @@ class MailingListCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView)
                 )
                 return self.form_invalid(form)
 
-        entry = form.save(commit=False)
+        entry = form.save()
         entry.mailing_list_log = f'>>> Record added by {self.request.user} on {get_current_date()} >>> Subscribed: {form.instance.subscribed}'
         entry.save(update_fields=['mailing_list_log',])
 
@@ -1745,8 +1745,8 @@ class MailingListUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView)
         else:
             message = f'{form.instance.audience} - {form.instance.email} ({get_current_date()})'
 
-        entry = form.save(commit=False)
-        entry.mailing_list_log = f'<br>>>> Record updated by {self.request.user} on {get_current_date()} >>> Subscribed: {form.instance.subscribed}'
+        entry = form.save()
+        entry.mailing_list_log = entry.mailing_list_log + f'<br>>>> Record updated by {self.request.user} on {get_current_date()} >>> Subscribed: {form.instance.subscribed}'
         entry.save(update_fields=['mailing_list_log',])
 
         messages.add_message(
