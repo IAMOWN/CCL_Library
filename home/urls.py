67a5b54af295149f6f711eb45edf54aecc0c509d
Carewen <carewen@protonmail.com>
2022-07-14 15:36:12 -0600

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('contact/', views.contact, name='contact'),
-    path('newsletter/', views.newsletter(), name='newsletter'),
+    path('newsletter/', views.newsletter, name='newsletter'),
 ]
