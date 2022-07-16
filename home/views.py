edc0d312a77de2c9fb6f61b54e3537edf7a4c91d
Carewen <carewen@protonmail.com>
2022-07-16 11:32:21 -0600

    Update views.py

--- home/views.py
+++ home/views.py
@@ -307,6 +307,7 @@ class SubscriptionCreate(CreateView):
         return context
 
     def form_valid(self, form):
+        new_subscription = form.save(commit=False)
         # Get IP Address
         client_ip, is_routable = get_client_ip(self.request)
         if client_ip is None:
@@ -319,8 +320,8 @@ class SubscriptionCreate(CreateView):
 
         print(f'client_ip: {client_ip}')
 
-        if self.request.user.is_anonymous:
-            print(f'self.request.user: {self.request.user}')
+        if self.request.user.is_anonymous:  # TODO Check for user account with this email address
+            # Check if email is already subscribed to this mailing list
             try:
                 email_exists = MailingList.objects.get(email=form.instance.email, audience=form.instance.audience)
                 print(f'email_exists: {email_exists}')
@@ -330,44 +331,33 @@ class SubscriptionCreate(CreateView):
                 )
                 return self.form_invalid(form)
 
+            # If the email is not already subscribed update the new subscription log with entry and change subscription status to Unconfirmed
             except MailingList.DoesNotExist:
-                print('Does not exist')
-                if form.instance.audience:
-                    if ip_result is None:
-                        new_subscription = MailingList.objects.create(
-                            audience=form.instance.audience,
-                            email=form.instance.email,
-                            subscribed='Unconfirmed',
-                            mailing_list_log=f'''>>> <strong>First Opt-In Subscription</strong> from <strong>IP: None</strong> on <strong>{get_current_year()}</strong>''',
-                        )
-                    elif ip_result:
-                        new_subscription = MailingList.objects.create(
-                            audience=form.instance.audience,
-                            email=form.instance.email,
-                            subscribed='Unconfirmed',
-                            mailing_list_log=f'''>>> <strong>First Opt-In Subscription</strong> from <strong>IP: {client_ip}</strong> on <strong>{get_current_year()}</strong>''',
-                        )
-                    elif ip_result == 'Private':
-                        new_subscription = MailingList.objects.create(
-                            audience=form.instance.audience,
-                            email=form.instance.email,
-                            subscribed='Unconfirmed',
-                            mailing_list_log=f'''>>> <strong>First Opt-In Subscription</strong> from <strong>IP: Private</strong> on <strong>{get_current_year()}</strong>''',
-                        )
+                new_subscription.subscribed = 'Unconfirmed'
+                if ip_result is None:
+                    new_subscription.mailing_list_log = f'''>>> <strong>First Opt-In Subscription</strong> from <strong>IP: None</strong> on <strong>{get_current_year()}</strong>'''
+                elif ip_result:
+                    new_subscription.mailing_list_log = f'''>>> <strong>First Opt-In Subscription</strong> from <strong>IP: {client_ip}</strong> on <strong>{get_current_year()}</strong>'''
+                elif ip_result == 'Private':
+                    new_subscription.mailing_list_log = f'''>>> <strong>First Opt-In Subscription</strong> from <strong>IP: Private</strong> on <strong>{get_current_year()}</strong>'''
+                new_subscription.save(update_fields=[
+                    'subscribed',
+                    'mailing_list_log',
+                ])
+                # Send subscription confirmation email
+                subject = f'Please confirm your subscription to the Cosmic Christ Love Newsletter {form.instance.audience} mailing list'
+                email_message = f'''
+                    {EMAIL_MESSAGE_CAMPAIGN_1}
+                    Beloved,<p>
+                    Bless You. Please confirm your subscription to the Cosmic Christ Love {form.instance.audience} mailing list by clicking this <a href="{CONFIRM_SUBSCRIPTION_URL}{form.instance.audience}/{form.instance.email}/">link</a>.<p>
+                    Love and Blessings,<br>
+                    The Elemental Grace Alliance.
+                    {EMAIL_MESSAGE_2}
+                '''
+                send_email(subject, form.instance.email, email_message)
 
-                    subject = f'Please confirm your subscription to the Cosmic Christ Love Newsletter {form.instance.audience} mailing list'
-                    email_message = f'''
-                        {EMAIL_MESSAGE_CAMPAIGN_1}
-                        Beloved,<p>
-                        Bless You. Please confirm your subscription to the Cosmic Christ Love {form.instance.audience} mailing list by clicking this <a href="{CONFIRM_SUBSCRIPTION_URL}{form.instance.audience}/{form.instance.email}/">link</a>.<p>
-                        Love and Blessings,<br>
-                        The Elemental Grace Alliance.
-                        {EMAIL_MESSAGE_2}
-                    '''
-                    send_email(subject, form.instance.email, email_message)
-
-                    subsciption_outcome_message = f'Bless You. An email to confirm your email subscription has been sent to {form.instance.email}. Love and Blessings.'
-
+                # Create flash message
+                subsciption_outcome_message = f'Bless You. An email to confirm your email subscription has been sent to {form.instance.email}. Love and Blessings.'
                 messages.add_message(
                     self.request,
                     messages.SUCCESS,
@@ -396,9 +386,11 @@ def subscription_confirm(request):
     # Process Second Opt-In request
     try:
         subscription = MailingList.objects.get(audience=audience, email=email)
+        # Check that the user has not already confirmed
         if subscription.subscribed == 'Yes':
             subsciption_outcome_message = f'The email "{email}" is already subscribed to the {audience} mailing list. There is nothing more you need do.<p>Love and Blessings,<br>The Elemental Grace Alliance'
             subscription_confirmed = True
+        # Add the Second Opt-in event to the mailing_list_log and update the subscription status
         else:
             subscription.subscribed = 'Yes'
             subscription_confirmed = True
@@ -412,18 +404,13 @@ def subscription_confirm(request):
                 'subscribed',
                 'mailing_list_log',
             ])
+
+    # The First Opt-In stap has not been completed. Direct the user to the subscription page.
     except MailingList.DoesNotExist:
         subsciption_outcome_message = f'''The email "{email} is not currently subscribed to the {audience} mailing list. If you wish to subscribe this email then please do so <a href="{SUBSCRIPTION_URL}" class="">here</a>.<p>Love and Blessings,<br>The Elemental Grace Alliance'''
 
-    if subscription_confirmed:
-        context = {
-            'subsciption_outcome_message': subsciption_outcome_message,
-            'title': f'Your Subscription Is Confirmed',
-        }
-    else:
-        context = {
-            'subsciption_outcome_message': subsciption_outcome_message,
-            'title': f'Your Subscription',
-        }
-
+    context = {
+        'subsciption_outcome_message': subsciption_outcome_message,
+        'title': f'Your Subscription Is Confirmed',
+    }
     return render(request, 'home/subscription_confirm.html', context)
