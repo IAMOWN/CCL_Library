9ed68d6ec34aed679fa9646d9f22c0501212408a
Carewen <carewen@protonmail.com>
2022-07-17 13:04:30 -0600

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     contact,
     newsletter,
-    # NewsletterDetailView,
+    NewsletterDetailView,
     SubscriptionCreate,
     subscription_confirm,
 )
