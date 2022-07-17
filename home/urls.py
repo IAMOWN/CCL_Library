87ca0767b2066066d6828c15be6dd39384fa7878
Carewen <carewen@protonmail.com>
2022-07-17 12:41:23 -0600

    Build newsletter detail page

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('newsletter/', views.newsletter, name='newsletter'),
+    path('newsletter/<int:pk>/', views.NewsletterDetailView.as_view(), name='newsletter-detail'),
     path('subscribe/', views.SubscriptionCreate.as_view(), name='subscribe'),
-    path('confirm_subscription/<str:audience>/<str:email>/', views.subscription_confirm, name='confirm-subscription')
+    path('confirm_subscription/<str:audience>/<str:email>/', views.subscription_confirm, name='confirm-subscription'),
 ]
