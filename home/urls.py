9d5a3f37619383fd621c2d8bdf324541c22b92c7
Carewen <carewen@protonmail.com>
2022-07-17 13:37:06 -0600

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from .views import (
     contact,
     newsletter,
     # NewsletterDetailView,
+    newsletter_detail,
     SubscriptionCreate,
     subscription_confirm,
 )
@@ -12,6 +13,7 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('newsletter/', newsletter, name='newsletter'),
     # path('newsletter/<int:pk>/', NewsletterDetailView.as_view(), name='newsletter-detail'),
+    path('newsletter/<int:pk>', newsletter_detail, name='newsletter-detail'),
     path('subscribe/', SubscriptionCreate.as_view(), name='subscribe'),
     path('confirm_subscription/<str:audience>/<str:email>/', subscription_confirm, name='confirm-subscription'),
 ]
