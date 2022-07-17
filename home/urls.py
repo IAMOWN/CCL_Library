553ee9ba3e3a0c1db75a497ec53143bfa9180e83
Carewen <carewen@protonmail.com>
2022-07-17 13:40:10 -0600

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import (
     contact,
     newsletter,
-    # NewsletterDetailView,
-    newsletter_detail,
+    NewsletterDetailView,
+    # newsletter_detail,
     SubscriptionCreate,
     subscription_confirm,
 )
@@ -12,8 +12,8 @@ from .views import (
 urlpatterns = [
     path('contact/', contact, name='contact'),
     path('newsletter/', newsletter, name='newsletter'),
-    # path('newsletter/<int:pk>/', NewsletterDetailView.as_view(), name='newsletter-detail'),
-    path('newsletter/<int:pk>', newsletter_detail, name='newsletter-detail'),
+    path('newsletter/<int:pk>/', NewsletterDetailView.as_view(), name='newsletter-detail'),
+    # path('newsletter/<int:pk>/', newsletter_detail, name='newsletter-detail'),
     path('subscribe/', SubscriptionCreate.as_view(), name='subscribe'),
     path('confirm_subscription/<str:audience>/<str:email>/', subscription_confirm, name='confirm-subscription'),
 ]
