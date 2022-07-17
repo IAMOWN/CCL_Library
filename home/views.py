20feba777c3effbbbd7e55edd632c38bd6da668f
Carewen <carewen@protonmail.com>
2022-07-16 19:25:46 -0600

    Build newsletter

--- home/views.py
+++ home/views.py
@@ -219,7 +219,7 @@ def release_notes(request):
 
 # ####################### Newsletter #######################
 def newsletter(request):
-    last_newsletter = EmailCampaign.objects.filter(audience__audience='CCL Newsletter', send_status='Sent').last()
+    last_newsletter = EmailCampaign.objects.filter(audience__audience='CCL Newsletter', send_status='Sent').last('date_published')
     newsletters = EmailCampaign.objects.filter(audience__audience='CCL Newsletter', send_status='Sent').order_by('-date_published')[:NUMBER_OF_NEWSLETTERS]
     context = {
         'newsletters': newsletters,
